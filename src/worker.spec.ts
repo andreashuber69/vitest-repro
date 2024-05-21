00436fbce87c95aef0621585a5c722c8a4de7de7
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-05-21 10:22:40 +0200

    feat: remove web-worker

--- src/worker.spec.ts
+++ src/worker.spec.ts
@@ -1,31 +1,17 @@
 // https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
 import { describe, expect, it } from "vitest";
-import Worker from "web-worker";
 
 const bounce = async (worker: Worker, val: boolean) => {
     let currentResolve: ((value: boolean) => void) | undefined;
-    let currentReject: ((err: unknown) => void) | undefined;
 
     const onMessage = (ev: MessageEvent<boolean>) => {
         currentResolve?.(ev.data);
-        remove();
-    }
-
-    const onError = (err: unknown) => {
-        currentReject?.(err);
-        remove();
-    }
-
-    const remove = () => {
         worker.removeEventListener("message", onMessage);
-        worker.removeEventListener("error", onError);
     }
 
-    return await new Promise<boolean>((resolve, reject) => {
+    return await new Promise<boolean>((resolve) => {
         currentResolve = resolve;
-        currentReject = reject;
         worker.addEventListener("message", onMessage);
-        worker.addEventListener("error", onError);
         worker.postMessage(val);
     });
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,4 @@ import { defineConfig } from "vitest/config";
 
 // eslint-disable-next-line import/no-anonymous-default-export, import/no-default-export
 export default defineConfig({
-    test: {
-        coverage: {
-            provider: "istanbul",
-            include: ["src/*.ts"],
-            reporter: ["lcov", "text"],
-        },
-    },
 });
